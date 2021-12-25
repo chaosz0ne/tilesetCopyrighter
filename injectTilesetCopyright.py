78176b1b6dcb3d98ad23c9f11b82f3a5d489a6fb
Robert <robertofon@os23.de>
2021-12-26 00:32:25 +0100

    test

--- injectTilesetCopyright.py
+++ injectTilesetCopyright.py
@@ -1,3 +1,16 @@
+import json
+
+print("Tiled-Editor Copyright-Info nach rc3.world-Vorgaben hinzufügen")
+print("Datei rechte.json passend befüllen, dann Aufruf")
+print("{$0} <tiledmapdatei.json>")
+print("Die <tiledmapdatei.json> wird modifiziert! Vorher eigene Änderungen committen!")
+print("")
+
 with open("rechte.json", "r") as rechte_file:
     rechte = json.load(rechte_file)
+    print("Folgende Rechte wurden zugeordnet:")
+    for recht in rechte:
+        print(recht["name"] + " zu -> " + recht["copytxt"])
+    #json.dump(rechte, rechte_file, indent=2)
 
+    with open()
